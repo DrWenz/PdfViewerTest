56a5968b7948e31f4e656b18a28e44bb6bc1375c
Markus Wenzl <markus.wenzl@mmw.solutions.com>
2020-02-24 22:16:57 +0100

    Projektdateien hinzufügen.

---
+++ PdfViewer/PDFViewer.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Resources;
+using System.Windows.Threading;
+using Windows.Data.Pdf;
+using Windows.Storage.Streams;
+
+namespace PdfViewer
+{
+    class PDFViewer : Control
+    {
+        public static readonly DependencyProperty PagesProperty = DependencyProperty.Register("Pages", typeof(ObservableCollection<PdfPage>), typeof(PDFViewer), new PropertyMetadata(null));
+        public ObservableCollection<PdfPage> Pages
+        {
+            get => (ObservableCollection<PdfPage>)this.GetValue(PagesProperty);
+            private set => this.SetValue(PagesProperty, value);
+        }
+        public PDFViewer()
+        {
+            ScrollerTimer = new DispatcherTimer();
+            ScrollerTimer.Interval = TimeSpan.FromMilliseconds(250);
+            ScrollerTimer.Tick += ScrollerTimer_Tick;
+
+            ThumbnailScrollerTimer = new DispatcherTimer();
+            ThumbnailScrollerTimer.Interval = TimeSpan.FromMilliseconds(250);
+            ThumbnailScrollerTimer.Tick += ThumbnailScrollerTimer_Tick; ;
+
+            this.Loaded += (s, e) => Test();
+        }
+
+        
+
+        private DispatcherTimer ScrollerTimer;
+        private DispatcherTimer ThumbnailScrollerTimer;
+
+        ScrollViewer thumbnailViewer;
+        ScrollViewer Scroller;
+        public override void OnApplyTemplate()
+        {
+            thumbnailViewer = this.GetTemplateChild("ThumbnailScroller") as ScrollViewer;
+            thumbnailViewer.ScrollChanged += ThumbnailViewer_ScrollChanged;
+
+            Scroller = this.GetTemplateChild("Scroller") as ScrollViewer;
+            Scroller.ScrollChanged += Scroller_ScrollChanged; ;
+
+            base.OnApplyTemplate();
+        }
+
+        private void Scroller_ScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            ScrollerTimer.Stop();
+            ScrollerTimer.Start();
+        }
+
+        private void ScrollerTimer_Tick(object sender, EventArgs e)
+        {
+            ScrollerTimer.Stop();
+
+            if (Pages == null)
+                return;
+
+            double viewOffset = Scroller.VerticalOffset;
+            double viewHeight = Scroller.ActualHeight;
+            double position = 0;
+
+            foreach (var page in Pages)
+            {
+                double pageOffset = position;
+                if ((pageOffset >= viewOffset && (pageOffset + page.ImageSize.Height) <= (viewOffset + viewHeight)) |
+                    (pageOffset <= viewOffset && (pageOffset + page.ImageSize.Height > viewOffset)) |
+                    (pageOffset >= viewOffset && pageOffset <= (viewOffset + viewHeight)))
+                    page.CreateImage();
+                else
+                    page.DeleteImage();
+
+                position += page.ImageSize.Height + 40;
+            }
+        }
+
+        private void ThumbnailViewer_ScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            ThumbnailScrollerTimer.Stop();
+            ThumbnailScrollerTimer.Start();
+        }
+
+        private void ThumbnailScrollerTimer_Tick(object sender, EventArgs e)
+        {
+            if (Pages == null)
+                return;
+
+            double viewOffset = thumbnailViewer.VerticalOffset;
+            double viewHeight = thumbnailViewer.ActualHeight;
+            double position = 0;
+
+            foreach (var page in Pages)
+            {
+                double pageOffset = position;
+                if ((pageOffset >= viewOffset && (pageOffset + page.ThumbnailSize.Height) <= (viewOffset + viewHeight)) |
+                    (pageOffset <= viewOffset && (pageOffset + page.ThumbnailSize.Height > viewOffset)) |
+                    (pageOffset >= viewOffset && pageOffset <= (viewOffset + viewHeight)))
+                    page.CreateThumbnail();
+                else
+                    page.DeleteThumbnail();
+
+                position += page.ThumbnailSize.Height + 40;
+            }
+        }
+
+        async void Test()
+        {
+            Pages = new ObservableCollection<PdfPage>();
+
+            string uri = "/PdfViewer;component/Manuals/OXYBABY_6_DE.pdf";
+            byte[] pdfData = null;
+            StreamResourceInfo streamInfo = Application.GetResourceStream(
+                new Uri(uri, UriKind.RelativeOrAbsolute));
+            if (streamInfo != null)
+            {
+                using (Stream stream = streamInfo.Stream)
+                {
+                    using (MemoryStream MS = new MemoryStream())
+                    {
+                        stream.CopyTo(MS);
+                        pdfData = MS.ToArray();
+                    }
+                }
+            }
+
+            PdfDocument doc = await PdfDocument.LoadFromStreamAsync(await ConvertTo(pdfData));
+            for (int i = 0; i < doc.PageCount; i++)
+            {
+                Pages.Add(new PdfPage(i, doc));
+            }
+
+        }
+
+        static PDFViewer()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(PDFViewer),
+                new FrameworkPropertyMetadata(typeof(PDFViewer)));
+        }
+
+        internal async Task<IRandomAccessStream> ConvertTo(byte[] arr)
+        {
+            InMemoryRandomAccessStream randomAccessStream = new InMemoryRandomAccessStream();
+            await randomAccessStream.WriteAsync(arr.AsBuffer());
+            randomAccessStream.Seek(0);
+            return (IRandomAccessStream)randomAccessStream;
+        }
+    }
+}
---
+++ PdfViewer/PdfPage.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using Windows.Data.Pdf;
+using Windows.Storage.Streams;
+
+namespace PdfViewer
+{
+    class PdfPage : INotifyPropertyChanged
+    {
+        #region Private Deklaration
+        private BitmapImage PropThumbnailImage;
+        private BitmapImage PropRenderedImage;
+        #endregion
+
+        #region Public Deklaration
+        public int Index { get; }
+        public Size ThumbnailSize { get; }
+        public Size ImageSize { get; }
+        public PdfDocument Document { get; }
+
+        public BitmapImage ThumbnailImage
+        {
+            get => PropThumbnailImage;
+            set
+            {
+                PropThumbnailImage = value;
+                OnPropertyChanged("ThumbnailImage");
+            }
+        }
+        public BitmapImage RenderedImage
+        {
+            get => PropRenderedImage;
+            set
+            {
+                PropRenderedImage = value;
+                OnPropertyChanged("RenderedImage");
+            }
+        }
+
+        public PdfPage(int index, PdfDocument document)
+        {
+            Index = index;
+            Document = document;
+
+            Windows.Data.Pdf.PdfPage page = Document.GetPage((uint)Index);
+            double aspectRatio = page.Size.Width / page.Size.Height;
+            ThumbnailSize = new Size(200, 200 / aspectRatio);
+            ImageSize = new Size(page.Size.Width, page.Size.Height);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string property)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+        #endregion
+
+        #region Constructor<
+
+        #endregion
+
+        public async void CreateThumbnail()
+        {
+            if(ThumbnailImage == null)
+                ThumbnailImage = await RenderPage(200, double.NaN);
+        }
+
+        public void DeleteThumbnail()
+        {
+            ThumbnailImage = null;
+        }
+
+        public async void CreateImage()
+        {
+            if(RenderedImage == null)
+                RenderedImage = await RenderPage(ImageSize.Width, ImageSize.Height);
+        }
+
+        public void DeleteImage()
+        {
+            RenderedImage = null;
+        }
+
+
+
+
+        private async Task<BitmapImage> RenderPage(double width, double height)
+        {
+            Windows.Data.Pdf.PdfPage page = Document.GetPage((uint)Index);
+
+            double aspectRatio = page.Size.Width / page.Size.Height;
+            Size renderedSize = new Size(width, height);
+            if (double.IsNaN(width) && !double.IsNaN(height))
+            {
+                renderedSize.Width = height * aspectRatio;
+            }
+            else if (double.IsNaN(height) && !double.IsNaN(width))
+            {
+                renderedSize.Height = width / aspectRatio;
+            }
+
+            BitmapImage image = new BitmapImage();
+            using (var stream = new InMemoryRandomAccessStream())
+            {
+                PdfPageRenderOptions opt = new PdfPageRenderOptions();
+                opt.DestinationHeight = Convert.ToUInt32(renderedSize.Height);
+                opt.DestinationWidth = Convert.ToUInt32(renderedSize.Width);
+                await page.RenderToStreamAsync(stream, opt);
+
+                image.BeginInit();
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.StreamSource = stream.AsStream();
+                image.DecodePixelHeight = Convert.ToInt32(renderedSize.Height);
+                image.DecodePixelWidth = Convert.ToInt32(renderedSize.Width);
+                image.EndInit();
+            }
+
+            image.Freeze();
+            return image;
+        }
+    }
+}
